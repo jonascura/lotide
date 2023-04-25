f3416c374ac506377c3917c4b3cbd76bd9d1ea8e
Jonas Cura <jonasm.cura@gmail.com.com>
2023-04-25 00:40:38 -0600

    Create assertEqual-esque function using eqArrays function

---
+++ assertArraysEqual.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(a, b) {
+  return JSON.stringify(a) === JSON.stringify(b); // turns arrays into strings for comparison
+};
+
+const assertArraysEqual = function(a, b) {
+  if (eqArrays(a, b) === true) {
+    console.log("Assertion Passed:", a, "===", b);
+  } else {
+    console.log("Assertion Failed:", a, "!==", b);
+  }
+};
+
+// TEST CODE
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+console.log();
+console.log("assertArraysEqual test:");
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
