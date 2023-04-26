3991dff8552f6034c6b415ce50bce04377d20664
Jonas Cura <jonasm.cura@gmail.com.com>
2023-04-25 22:28:40 -0600

    Added proper countOnly function that works with assertequal function

--- countOnly.js
+++ countOnly.js
@@ -10,34 +10,19 @@ const assertEqual = function(actual, expected) {
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
-  // Has to check all values of original array
-  // Compares itemsToCount to allItems
-  // if item is there, return all values of item repetition
-  
-  // check itemsToCount
-  for (item in itemsToCount) {
-    
-    //console.log(item) // Name value
-    //console.log(itemsToCount[item]) // Truthy or falsey
-    if (itemsToCount[item] === true) {
-      count = 0
-      for (index in allItems) {
-        // console.log(index) // Number index
-        // console.log(allItems[index]) // name value
-        if (item === allItems[index]) {
-          // console.log('They match here')
-          count += 1;
-        }
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
       }
-      if (count > 0) {
-        console.log(item + ':', count);
-      }   
     }
   }
+  return results;
 };
-
-
-
 
 const firstNames = [
   "Karl",
@@ -51,4 +36,9 @@ const firstNames = [
   "Joe"
 ];
 
-countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined); // because there is no name in firstNames
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined); // because name is false in check
