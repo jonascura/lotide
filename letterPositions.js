2fb6ccc924875cc4a4bb3d1fa70dcebcef34c8ad
Jonas Cura <jonasm.cura@gmail.com.com>
2023-04-27 00:41:03 -0600

    Added letterPositions function that returns array and assertArrays function to check assertion

--- letterPositions.js
+++ letterPositions.js
@@ -25,18 +25,26 @@ const letterPositions = function(sentence) {
   // logic to update results here
   for (let index in sentence) { // Catches letters in sentence
     if (!results[sentence[index]]) { // if sentence[index] (or letter) has not been captured yet
-      results[sentence[index]] = index
+      results[sentence[index]] = [Number(index)];
     } else {
-      results[sentence[index]] += ', ' + index
+      results[sentence[index]].push(Number(index)); // its adding the numbers together
     }
   }
   return results;
 };
 
-const result = countLetters("HELLO");
-console.log(result);
-assertEqual(result["H"], 1);
+const eqArrays = function(a, b) {
+  return JSON.stringify(a) === JSON.stringify(b); // turns arrays into strings for comparison
+};
+
+const assertArraysEqual = function(a, b) {
+  if (eqArrays(a, b) === true) {
+    console.log("Assertion Passed:", a, "===", b);
+  } else {
+    console.log("Assertion Failed:", a, "!==", b);
+  }
+};
 
 const result1 = letterPositions("lighthouse in the house");
 console.log(result1);
-assertEqual(result1["l"], 0);
+assertArraysEqual(result1["n"], [12]);
