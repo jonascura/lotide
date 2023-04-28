b4b83f0c8d5bdf416635d8a442fb3b5076dcbd73
Jonas Cura <jonasm.cura@gmail.com.com>
2023-04-28 00:46:32 -0600

    Removed console.log steps, only assertions printed

--- eqObjects.js
+++ eqObjects.js
@@ -19,25 +19,16 @@ const eqObjects = function(object1, object2) {
     let isTrue = false;
     // iterate through the keys and values
     for (let key1 of Object.keys(object1)) {
-      console.log("the key we are checking is:", key1); // visualize current key
       // check if key is an array or primative
       if (Array.isArray(object1[key1])) {
-        console.log("this key is an ARRAY!!"); // confirmation of array
-        console.log("arrays are", object1[key1], object2[key1]); // visualize arrays
         if (!eqArrays(object1[key1], object2[key1])) { // if not true
-          console.log("the arrays DO NOT match");
           return false; // return false and terminate function
         } else {
-          console.log("the arrays match");
           isTrue = true; // can't use return because it terminates the function not allowing loop if needed, change isTrue value instead
         }
       } else if (object1[key1] === object2[key1]) {
-        console.log("it's primative"); // confirm not array
-        console.log("the values are:", object1[key1] + ',', object2[key1]); // visualize values
-        console.log("primatives match"); // confirm match
         isTrue = true; // can't use return because it terminates the function not allowing loop if needed, change isTrue value instead
       } else {
-        console.log("primatives DO NOT match"); // confirm not match
         return false; // terminate function once false
       }
     }
@@ -47,26 +38,19 @@ const eqObjects = function(object1, object2) {
   }
 };
 
-console.log('same size and color >> true');
 const multiColorShirtObject = {  colors: ["red", "blue"], size: "medium"};
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
 //eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject); // => true
 assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), true);
 
-console.log();
-console.log('same size wrong color >> false');
 const aWrongMultiColorShirtObject = { size: "medium", colors: ["red", "green"] };
 //eqObjects(multiColorShirtObject  , aWrongMultiColorShirtObject); // => false
 assertEqual(eqObjects(multiColorShirtObject  , aWrongMultiColorShirtObject), false);
 
-console.log();
-console.log('wrong size right color >> false');
 const aWrongSizedMultiColorShirtObject = { size: "large", colors: ["red", "blue"] };
 //eqObjects(multiColorShirtObject  , aWrongSizedMultiColorShirtObject); // => false
 assertEqual(eqObjects(multiColorShirtObject  , aWrongSizedMultiColorShirtObject), false);
 
-console.log();
-console.log("keys don't match >> false");
 const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
 //eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject); // => false
 assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false);
