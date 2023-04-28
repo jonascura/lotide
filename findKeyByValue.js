2753533a8559f6fcbdd5ead8e55103a6ec6f90da
Jonas Cura <jonasm.cura@gmail.com.com>
2023-04-27 19:30:53 -0600

    Added findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  // Grab all the keys [ 'sci_fi', 'comedy', 'drama' ]
+  for (let key of Object.keys(object)) {
+    // grab key values
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
