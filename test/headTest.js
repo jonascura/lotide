a5289ca510d2f1f2ea26e244fbb90b02c6022a06
Jonas Cura <jonasm.cura@gmail.com.com>
2023-05-10 20:16:41 -0600

    Add test scenario

--- test/headTest.js
+++ test/headTest.js
@@ -9,5 +9,9 @@ describe("#head", () => {
   it("returns '5' for ['5']", () => {
     assert.strictEqual(head(['5']), '5'); 
   });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
   
 });
