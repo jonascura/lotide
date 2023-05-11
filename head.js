79072bb22c4b504336540d65a83ccfbba52bfc08
Jonas Cura <jonasm.cura@gmail.com.com>
2023-05-10 18:10:02 -0600

    Modularize code

--- head.js
+++ head.js
@@ -1,20 +1,8 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
+// FUNCTION IMPLEMENTATION
 const head = function(array) {
   return array[0];
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports = head;
