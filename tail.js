891bf06f9805456f11a51b8e601262d1c566706c
Jonas Cura <jonasm.cura@gmail.com.com>
2023-05-10 18:26:56 -0600

    Modularized code

--- tail.js
+++ tail.js
@@ -1,12 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
+// FUNCTION IMPLEMENTATION
 const tail = function(array) {
   let tailArray = [];
   for (let i = 1; i < array.length; i++) {
@@ -15,7 +9,4 @@ const tail = function(array) {
   return tailArray;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
